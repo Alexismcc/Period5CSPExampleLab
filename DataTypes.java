2c85bda3cdf2a24d7621587e1b71bf97e2a4a7a4
Alexis <alexis.mccarthy@vtsd.com>
2022-09-09 12:28:56 -0400

    Data Type Update

---
+++ DataTypes.java
@@ -0,0 +1,15 @@
+// Single Line Comment Syntax
+/*
+    Multi-line comment syntax.
+
+    Nothing in here will be executed (ran)
+
+ */
+public class DataTypes {
+
+    //Variable Syntax:  dataType varName = Value;
+    int FavInt = 13;
+    double snicker = 1.3;
+    Boolean likeDog = true;
+
+}
